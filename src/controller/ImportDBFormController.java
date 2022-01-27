c97240d50451dfd030404548bd1641bcafa17d35
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-01-27 10:18:49 +0530

    Create the splash screen

--- src/controller/ImportDBFormController.java
+++ src/controller/ImportDBFormController.java
@@ -1,4 +1,48 @@
 package controller;
 
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
 public class ImportDBFormController {
+    public RadioButton rdoRestore;
+    public ToggleGroup abc;
+    public TextField txtPath;
+    public Button btnBrowse;
+    public RadioButton rdoFirstBoot;
+    public Button btnOk;
+    private SimpleObjectProperty<File> fileProperty;
+
+    public void initialize () {
+        rdoRestore.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            btnOk.setDisable(txtPath.getText().isEmpty() && newValue);
+        });
+    }
+
+    public void btnBrowseOnAction(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select a Backup File");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Backup Files", "*.dep8backup"));
+        File file = fileChooser.showOpenDialog(btnBrowse.getScene().getWindow());
+        txtPath.setText(file != null ? file.getAbsolutePath() : "");
+        fileProperty.setValue(file);
+    }
+
+    public void btnOkOnAction(ActionEvent actionEvent) {
+        if (rdoFirstBoot.isSelected()) {
+            fileProperty.setValue(null);
+        }
+        ((Stage) btnOk.getScene().getWindow()).close();
+    }
+
+    public void initFileProperty(SimpleObjectProperty<File> fileProperty) {
+        this.fileProperty = fileProperty;
+    }
 }
--- src/controller/SplashScreenController.java
+++ src/controller/SplashScreenController.java
@@ -1,4 +1,105 @@
 package controller;
 
+import javafx.application.Platform;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class SplashScreenController {
+    public Label lblStatus;
+
+    public void initialize() {
+        establishDBConnection();
+    }
+
+    private void establishDBConnection() {
+        new Thread(() -> {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                DriverManager.getConnection("jdbc:mysql://localhost:3306/student_attendance_system", "root", "mysql");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                if (e.getSQLState().equals("42000")) {
+                    Platform.runLater(() -> loadImportDBForm());
+                }
+            }
+        }).start();
+
+    }
+
+    private void loadImportDBForm() {
+        SimpleObjectProperty<File> fileProperty = new SimpleObjectProperty<>();
+        try {
+            Stage stage = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/view/ImportDBForm.fxml"));
+            AnchorPane root = fxmlLoader.load();
+            ImportDBFormController controller = fxmlLoader.getController();
+            controller.initFileProperty(fileProperty);
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.sizeToScene();
+            stage.setTitle("Student Attendance System : First time boot");
+            stage.setResizable(false);
+            stage.centerOnScreen();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initOwner(lblStatus.getScene().getWindow());
+            stage.showAndWait();
+
+            if (fileProperty.getValue() == null) {
+                /*Create new database*/
+                lblStatus.setText("Creating a new Database");
+
+                new Thread(() -> {
+                    try {
+                        sleep(100);
+                        Platform.runLater(() -> lblStatus.setText("Loading Database script"));
+                        InputStream inputStream = this.getClass().getResourceAsStream("/assets/db-script.sql");
+                        byte[] buffer = new byte[inputStream.available()];
+                        inputStream.read(buffer);
+                        String script = new String(buffer);
+
+                        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306?allowMultiQueries=true", "root", "mysql");
+                        Platform.runLater(() -> lblStatus.setText("Executing Database Script"));
+                        Statement statement = connection.createStatement();
+                        statement.execute(script);
+                        connection.close();
+                        sleep(100);
+
+                        Platform.runLater(() -> lblStatus.setText("Obtaining a new Database connection"));
+                        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "mysql");
+                        sleep(100);
+
+                        Platform.runLater(() -> lblStatus.setText("Setting up the UI"));
+                    } catch (IOException | SQLException e) {
+                        e.printStackTrace();
+                    }
+                }).start();
+            } else {
+                /*Todo : Restore the backup*/
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
