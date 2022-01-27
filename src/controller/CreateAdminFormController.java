da98d416ff7dad5da822313245be95184589d78a
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-01-27 22:17:56 +0530

    Create 'Create Admin Form' UI

---
+++ src/controller/CreateAdminFormController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Rectangle;
+
+public class CreateAdminFormController {
+    public TextField txtName;
+    public TextField txtUsername;
+    public Rectangle rctOne;
+    public Rectangle rctTwo;
+    public Rectangle rctThree;
+    public Rectangle rctFour;
+    public Rectangle rctFive;
+    public PasswordField txtPassword;
+    public PasswordField txtConfirmPassword;
+    public Button btnCreateAccount;
+
+    public void btnCreateAccountOnAction(ActionEvent actionEvent) {
+    }
+}
