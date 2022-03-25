97dbc2fe110b7af6129365d6385ed35c52c2ab7e
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-03-25 14:46:12 +0530

    finalize the backup feature in login form

--- src/controller/ImportDBFormController.java
+++ src/controller/ImportDBFormController.java
@@ -2,14 +2,12 @@ package controller;
 
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 
 public class ImportDBFormController {
     public RadioButton rdoRestore;
@@ -29,7 +27,7 @@ public class ImportDBFormController {
     public void btnBrowseOnAction(ActionEvent actionEvent) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select a Backup File");
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Backup Files", "*.dep8backup"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Backup Files", "*.sasbackup"));
         File file = fileChooser.showOpenDialog(btnBrowse.getScene().getWindow());
         txtPath.setText(file != null ? file.getAbsolutePath() : "");
         fileProperty.setValue(file);
@@ -38,6 +36,27 @@ public class ImportDBFormController {
     public void btnOkOnAction(ActionEvent actionEvent) {
         if (rdoFirstBoot.isSelected()) {
             fileProperty.setValue(null);
+        }
+        if (fileProperty.getValue()!=null){
+            ProcessBuilder mysqlProcessBuilder = new ProcessBuilder("mysql",
+                    "-h", "localhost",
+                    "--port", "3306",
+                    "-u", "root",
+                    "-pmysql");
+
+            mysqlProcessBuilder.redirectInput(fileProperty.getValue());// < file
+            try {
+                Process mysqlRestore = mysqlProcessBuilder.start();
+                int exitCode = mysqlRestore.waitFor();
+
+                if (exitCode==0){
+                    new Alert(Alert.AlertType.INFORMATION, "Restore process successfully completed!").show();
+                } else {
+                    new Alert(Alert.AlertType.ERROR, "Restore process failed, please try again!").show();
+                }
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         ((Stage) btnOk.getScene().getWindow()).close();
     }
